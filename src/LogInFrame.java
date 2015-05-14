0d11e8f040cf22f4be6ca28f2f0f5bcfb873963c
r7852441 <r7852441@gmail.com>
2015-05-14 22:04:46 +0900

    v1.5
    視窗架構

--- src/LogInFrame.java
+++ src/LogInFrame.java
@@ -1,16 +1,20 @@
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
 import java.awt.GridLayout;
-import javax.swing.JPasswordField;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.border.EmptyBorder;
+
 
 
 public class LogInFrame extends JFrame {
@@ -20,88 +24,115 @@ public class LogInFrame extends JFrame {
 	public LogInFrame(LogIn login) {
 		initComp(login);
 	}
+	
+	private JButton jbtnClear1=new JButton("Clear");
+	private JButton jbtnClear2=new JButton("Clear");
+	private JLabel jlb1=new JLabel("ID:");
+	private JLabel jlb2=new JLabel("PIN:");
+	private JTextField jtfAcc=new JTextField("b00001");
+	private JPasswordField jpfPass=new JPasswordField("458712");
+	private JButton jbtnNumber1=new JButton("1");
+	private JButton jbtnNumber2=new JButton("2");
+	private JButton jbtnNumber3=new JButton("3");
+	private JButton jbtnNumber4=new JButton("4");
+	private JButton jbtnNumber5=new JButton("5");
+	private JButton jbtnNumber6=new JButton("6");
+	private JButton jbtnNumber7=new JButton("7");
+	private JButton jbtnNumber8=new JButton("8");
+	private JButton jbtnNumber9=new JButton("9");
+	private JButton jbtnNumbera=new JButton("");
+	private JButton jbtnNumber0=new JButton("0");
+	private JButton jbtnNumberb=new JButton("");
+	private JButton jbtnCancel=new JButton("Cancel");
+	private JButton jbtnEnter=new JButton("Enter");
+	private Container cp;
+	private JPanel jp1 = new JPanel();
+	private JPanel jp2 = new JPanel();
+	private JPanel jp3 = new JPanel();
+	private JPanel jp4 = new JPanel();
+	private boolean tt = false;
+	
+	public boolean status(){
+		return tt;
+	}
+	
 	private void initComp(final LogIn login) {
 		
-		JPanel NORTH = new JPanel();
-		getContentPane().add(NORTH, BorderLayout.NORTH);
-		NORTH.setLayout(new GridLayout(0, 3, 2, 5));
+		setVisible(true);
+		this.setTitle("LogIn");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(true);
+		setBounds(600,600,700,400);
+		cp = this.getContentPane();
+		cp.setLayout(new BorderLayout());
+		cp.setBackground(Color.orange);
 		
-		JLabel lblNewLabel = new JLabel("Account");
-		NORTH.add(lblNewLabel);
+		cp.add(jp1,BorderLayout.NORTH);
+		cp.add(jp2,BorderLayout.CENTER);
 		
-		textField = new JTextField();
-		NORTH.add(textField);
-		textField.setColumns(10);
+		jp1.setLayout(new GridLayout(0,3,2,5));
 		
-		JButton btnNewButton_3 = new JButton("Clear");
-		NORTH.add(btnNewButton_3);
+		jp1.add(jlb1);
+		jp1.add(jtfAcc);
+		jp1.add(jbtnClear1);
+		jp1.add(jlb2);
+		jp1.add(jpfPass);
+		jp1.add(jbtnClear2);
 		
-		JLabel lblNewLabel_1 = new JLabel("PIN");
-		NORTH.add(lblNewLabel_1);
+		jp2.setLayout(new BorderLayout());
 		
-		passwordField = new JPasswordField();
-		NORTH.add(passwordField);
+		jp2.add(jp3,BorderLayout.CENTER);
+		jp2.add(jp4,BorderLayout.EAST);
 		
-		JButton btnNewButton_14 = new JButton("Clear");
-		NORTH.add(btnNewButton_14);
+		jp3.setLayout(new GridLayout(0, 3, 4, 5));
 		
-		JPanel CENTER = new JPanel();
-		getContentPane().add(CENTER, BorderLayout.CENTER);
-		CENTER.setLayout(new BorderLayout(2, 0));
+		jp3.add(jbtnNumber1);
+		jp3.add(jbtnNumber2);
+		jp3.add(jbtnNumber3);
+		jp3.add(jbtnNumber4);
+		jp3.add(jbtnNumber5);
+		jp3.add(jbtnNumber6);
+		jp3.add(jbtnNumber7);
+		jp3.add(jbtnNumber8);
+		jp3.add(jbtnNumber9);
+		jp3.add(jbtnNumbera);
+		jp3.add(jbtnNumber0);
+		jp3.add(jbtnNumberb);
 		
-		JPanel EAST = new JPanel();
-		CENTER.add(EAST, BorderLayout.EAST);
-		EAST.setLayout(new GridLayout(0, 1, 2, 5));
+		jp4.setLayout(new GridLayout(0, 1, 2, 5));
 		
-		JButton btnNewButton_4 = new JButton("Cancel");
-		EAST.add(btnNewButton_4);
+		jp4.add(jbtnCancel);
+		jp4.add(jbtnEnter);  
 		
-		JButton btnNewButton_15 = new JButton("Enter");
-		EAST.add(btnNewButton_15);
 		
-		JPanel NUMBER = new JPanel();
-		CENTER.add(NUMBER, BorderLayout.CENTER);
-		NUMBER.setLayout(new GridLayout(0, 3, 4, 5));
 		
-		JButton btnNewButton = new JButton("1");
-		NUMBER.add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		jbtnClear1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				jtfAcc.setText("");
 			}
 		});
 		
-		JButton btnNewButton_1 = new JButton("2");
-		NUMBER.add(btnNewButton_1);
+		jbtnClear2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				jpfPass.setText("");
+			}
+		});
 		
-		JButton btnNewButton_2 = new JButton("3");
-		NUMBER.add(btnNewButton_2);
-		
-		JButton btnNewButton_5 = new JButton("4");
-		NUMBER.add(btnNewButton_5);
-		
-		JButton btnNewButton_8 = new JButton("5");
-		NUMBER.add(btnNewButton_8);
-		
-		JButton btnNewButton_6 = new JButton("6");
-		NUMBER.add(btnNewButton_6);
-		
-		JButton btnNewButton_7 = new JButton("7");
-		NUMBER.add(btnNewButton_7);
-		
-		JButton btnNewButton_11 = new JButton("8");
-		NUMBER.add(btnNewButton_11);
-		
-		JButton btnNewButton_9 = new JButton("9");
-		NUMBER.add(btnNewButton_9);
-		
-		JButton btnNewButton_10 = new JButton("");
-		NUMBER.add(btnNewButton_10);
-		
-		JButton btnNewButton_12 = new JButton("0");
-		NUMBER.add(btnNewButton_12);
-		
-		JButton btnNewButton_13 = new JButton("");
-		NUMBER.add(btnNewButton_13);
+		jbtnEnter.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				String sAcc = jtfAcc.getText();
+				login.setAcc(sAcc);										
+				login.setPassWD(new String(jpfPass.getPassword()));		
+				if(login.findAccount()){								
+					JOptionPane.showMessageDialog(null,"Welcome "+login.getUserName()+" !");
+					setVisible(false);
+					tt = true;
+				}else{
+					JOptionPane.showMessageDialog(null,"Ops! "+sAcc+" please try again!");
+				}
+			}
+		});
+				
 	}
 
 }
