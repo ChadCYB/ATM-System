ce00e23befc3636bc3ec5c51d887b225f745bba4
ChadCYB <Chad.CYB@gmail.com>
2015-05-19 23:27:32 +0800

    V2.2
    GUI look change

--- src/main.java
+++ src/main.java
@@ -1,6 +1,17 @@
-
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 public class main {
 	public static void main(String[] args) {
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		Bank aBank = new Bank();
 		LogIn login = new LogIn(aBank);
 		ATM atm1 = new ATM(aBank);;
