15d5a6b15a864de5eaf5e5a79341124631d28bfc
ChadCYB <Chad.CYB@gmail.com>
2015-05-25 01:04:56 +0800

    V3.2
    Read ArrayList change to read SQL data
    sqlsearch method build

--- src/ATM.java
+++ src/ATM.java
@@ -6,18 +6,18 @@ public class ATM {
 	public ATM(Bank bank){
 		theBank = bank;
 	}
-	public boolean pickUpMoney(double money){	//領錢
-		return theBank.pickUpMoney(aID, aPIN, money);
-	}
-	public boolean saveMoney(double money){		//存錢
-		return theBank.saveMoney(aID, aPIN, money);
-	}
+//	public boolean pickUpMoney(double money){	//領錢
+//		return theBank.pickUpMoney(aID, aPIN, money);
+//	}
+//	public boolean saveMoney(double money){		//存錢
+//		return theBank.saveMoney(aID, aPIN, money);
+//	}
 	public boolean fdMoney(){					//定存
 		return false;	//<<<<NOT YET
 	}
-	public boolean moneyTransfer(double money, String TrfInID){		//匯款
-		return theBank.moneyTrf(aID, aPIN, money, TrfInID);
-	}
+//	public boolean moneyTransfer(double money, String TrfInID){		//匯款
+//		return theBank.moneyTrf(aID, aPIN, money, TrfInID);
+//	}
 	public void setAccUser(String sAcc, String sPass) {
 		aID = sAcc;
 		aPIN = sPass;
--- src/main.java
+++ src/main.java
@@ -20,9 +20,9 @@ public class main {
 		Bank aBank = new Bank();
 		LogIn login = new LogIn(aBank);
 		ATM atm1 = new ATM(aBank);
-		aBank.addAccount(new Account("兆博","A10546","b00001","458712",
+		aBank.addAccount(new Account("兆博","b00001","A10546","458712",
 				new BankAccount("123456",15000,5000)));
-		aBank.addAccount(new Account("小九","A12608","b00002","453354",
+		aBank.addAccount(new Account("小九","b00002","A12608","453354",
 				new BankAccount("231897",13534,1200)));
 		LogInFrame loginFrame = new LogInFrame(login, atm1);
 	}
