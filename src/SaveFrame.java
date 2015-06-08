adf57c394b9101fa5fb9fe3fcac9a34596ecbc14
Yi-Cheng Chen <Chad.CYB@gmail.com>
2015-06-08 15:47:27 +0800

    V5.7
    frame coding style change

--- src/SaveFrame.java
+++ src/SaveFrame.java
@@ -1,93 +1,70 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+
 import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 public class SaveFrame extends JFrame {
-	
 	private JPanel contentPane=new JPanel();
 	private JPanel north = new JPanel();
-	private JLabel now$ = new JLabel("目前金額:");
-	private JLabel money = new JLabel("金錢");
 	private JPanel center = new JPanel();
-	private JLabel lab$ = new JLabel("請輸入金額:");
-	private JTextPane textPane = new JTextPane();
 	private JPanel bottom = new JPanel();
+	private JLabel nowMoney = new JLabel("目前金額:");
+	private JLabel money = new JLabel("金錢");
+	private JLabel inMoney = new JLabel("請輸入金額:");
+	private JTextField jtf1 = new JTextField();
 	private JButton Confirm = new JButton("確認");
 	private JButton Break = new JButton("退回");
 	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SaveFrame frame = new SaveFrame(null);
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public SaveFrame(ATM atm) {
 		SaveFrame1(atm);
 	}
 	public void SaveFrame1(ATM atm) {
+		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(0, 1, 5, 5));
-		
 		north.setLayout(new GridLayout(1, 0, 5, 5));
-		
-		now$.setFont(new Font("新細明體", Font.BOLD, 22));
-		now$.setHorizontalAlignment(SwingConstants.CENTER);
-		
 		center.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		lab$.setFont(new Font("新細明體", Font.BOLD, 22));
-		lab$.setHorizontalAlignment(SwingConstants.CENTER);
+		nowMoney.setFont(new Font("新細明體", Font.BOLD, 22));
+		nowMoney.setHorizontalAlignment(SwingConstants.CENTER);
+
+		inMoney.setFont(new Font("新細明體", Font.BOLD, 22));
+		inMoney.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		bottom.setLayout(new GridLayout(1, 2, 5, 5));
 		
-		Confirm.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		
-		Break.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
 		contentPane.add(north);
-		north.add(now$);
+		north.add(nowMoney);
 		north.add(money);
 		contentPane.add(center);
-		center.add(lab$);
-		center.add(textPane);
+		center.add(inMoney);
+		center.add(jtf1);
 		contentPane.add(bottom);
 		bottom.add(Confirm);
 		bottom.add(Break);
 		
+		Confirm.addActionListener(new ActionListener() {		//確認
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		
+		Break.addActionListener(new ActionListener() {			//取消
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 	}
-
 }
